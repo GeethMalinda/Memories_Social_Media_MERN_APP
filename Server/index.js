94bd14796eba278cf3435b0f40016501a49946c5
geeth <geeth@misynergy.com>
2022-09-07 20:38:04 +0530

    index js setup

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express();
+
+app.use(bodyParser.json({limit:"30",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30",extended:true}))
+app.use(cors())
+
+
+const CONNECTION_URL = 'mongodb+srv://root:1234@cluster0.gaudcge.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+mongoose.connect(CONNECTION_URL,{
+
+    //for simplicity purpose
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(() => app.listen(PORT,() => console.log(`SERVER RUNNING ON ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+
+
+//https://www.mongodb.com/cloud/atlas
+//host our database on their cloud
+
+
