1fd391ea71110d67556f990e7afa8b647037e075
geeth <geeth@misynergy.com>
2022-09-13 22:44:40 +0530

    cross origin fix

--- Server/Controllers/posts.js
+++ Server/Controllers/posts.js
@@ -3,6 +3,11 @@ import PostMessage from '../Models/postMessage.js';
 export const getPosts = async (req,res) => {
     try{
         const postMessages = await postMessage.find();
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Max-Age", "1800");
+        res.setHeader("Access-Control-Allow-Headers", "content-type");
+        res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS")
         res.status(200).json(postMessages);
     }catch (e) {
         res.status(503).json({message:e.message})
--- Server/index.js
+++ Server/index.js
@@ -6,11 +6,19 @@ import postRoutes  from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts',postRoutes)
+
 
 app.use(bodyParser.json({limit:"30",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30",extended:true}))
-app.use(cors())
+
+app.use('/posts', postRoutes);
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 
 
 const CONNECTION_URL = 'mongodb+srv://root:1234@cluster0.gaudcge.mongodb.net/?retryWrites=true&w=majority'
