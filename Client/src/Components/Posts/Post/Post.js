52504cc5fb3cc3c977d31eb4d9475503e10f60f9
geeth <geeth@misynergy.com>
2022-10-29 10:57:03 +0530

    like post improvemts

--- Client/src/Components/Posts/Post/Post.js
+++ Client/src/Components/Posts/Post/Post.js
@@ -2,18 +2,31 @@ import React from 'react';
 import useStyles from './Styles';
 import {Button, Card, CardActions, CardContent, CardMedia, Typography} from '@mui/material';
 import moment from 'moment';
-import {Delete, MoreHoriz, ThumbUpAlt} from '@mui/icons-material';
+import {Delete, MoreHoriz, ThumbUpAlt,ThumbUpAltOutlined} from '@mui/icons-material';
 import {useDispatch} from 'react-redux';
 import {deletePost, likePost} from '../../../actions/posts';
 
 const Post = ({post,setCurrentId}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const Likes = () => {
+        if (post.likes.length > 0 ){
+            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+                ? (
+                    <><ThumbUpAlt fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+                ) : (
+                    <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+                );
+        }
+        return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    }
     return(
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
@@ -33,7 +46,11 @@ const Post = ({post,setCurrentId}) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAlt fontSize="small" /> Like {post.likeCount} </Button>
+                <Button disabled={!user?.result} size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+                    {/*<ThumbUpAlt fontSize="small" />*/}
+                {/*Like {post.likeCount} */}
+                    <Likes />
+                </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><Delete fontSize="small" /> Delete</Button>
             </CardActions>
         </Card>
