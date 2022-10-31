682407a00dfae47a172fe5daa8053934406fa4a3
geeth <geeth@misynergy.com>
2022-10-31 07:24:51 +0530

    not users post disable

--- Client/src/Components/Posts/Post/Post.js
+++ Client/src/Components/Posts/Post/Post.js
@@ -29,15 +29,22 @@ const Post = ({post,setCurrentId}) => {
                 <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            <div className={classes.overlay2}>
-                <Button
-                    style={{ color: 'white' }}
-                    size="small"
-                    onClick={() => setCurrentId(post._id)}
-                >
-                    <MoreHoriz fontSize="default" />
-                </Button>
-            </div>
+
+            {
+                (user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+
+                    <div className={classes.overlay2}>
+                        <Button
+                            style={{color: 'white'}}
+                            size="small"
+                            onClick={() => setCurrentId(post._id)}
+                        >
+                            <MoreHoriz fontSize="default"/>
+                        </Button>
+                    </div>
+                )
+            }
+
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
@@ -51,7 +58,14 @@ const Post = ({post,setCurrentId}) => {
                 {/*Like {post.likeCount} */}
                     <Likes />
                 </Button>
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><Delete fontSize="small" /> Delete</Button>
+                {
+                    (user?.result?.googleId === post?.creator ||  user?.result?._id === post?.creator) && (
+                        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} ><Delete fontSize="small" />
+                            Delete
+                        </Button>
+                    )
+                }
+
             </CardActions>
         </Card>
     )
