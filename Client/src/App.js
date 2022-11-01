71d4178049345914aa5c4f3f9f9af1bac447e524
geeth <geeth@misynergy.com>
2022-11-01 10:27:06 +0530

    improvement app routing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,24 @@
 import { Container} from '@mui/material';
 import Navbar from './Components/Navbar/NavBar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Auth from './Components/Auth/Auth';
 
 
 function App() {
 
-  return (
+    const  user = JSON.parse(localStorage.getItem('profile'))
+
+    return (
       <BrowserRouter>
           <Container maxWidth="lg">
               <Navbar />
               <Routes>
-                  <Route path="/" exact element={<Home/>} />
-                  <Route path="/auth" exact element={<Auth/>} />
+                  <Route path="/" exact element={<Navigate to="/posts" replace />}/>
+                  <Route path="/posts" exact element={<Home/>} />
+                  <Route path="/posts/search" exact element={<Home/>} />
+                  <Route path="/post/:id" exact element={<PostDetails/>} />
+                  <Route path="/auth" exact element={ !user ? <Auth/> : <Navigate to="/posts" replace /> } />
               </Routes>
           </Container>
       </BrowserRouter>
