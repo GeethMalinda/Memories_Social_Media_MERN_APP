ec4deed46898bc341d29fd2ae0ad156c6dc89075
geeth <geeth@misynergy.com>
2022-11-12 09:27:39 +0530

    improvements

--- Client/src/Components/Posts/Posts.js
+++ Client/src/Components/Posts/Posts.js
@@ -13,7 +13,7 @@ const Posts = ({setCurrentId}) => {
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {
                     posts.map((post) => (
-                        <Grid item key={post._id} xs={12} sm={6}>
+                        <Grid item key={post._id} xs={12} sm={12} md={4} lg={3}>
                             <Post post={post} setCurrentId = {setCurrentId}/>
                         </Grid>
                     ))
--- Client/src/reducers/auth.js
+++ Client/src/reducers/auth.js
@@ -3,14 +3,11 @@ import {AUTH, LOGOUT} from '../constants/actiontypes';
 const authReducer = (state = {authData:null}, action) => {
     switch (action.type){
         case AUTH:
-            console.log("reducer action data auth ==>",action?.data);
             localStorage.setItem('profile',JSON.stringify({...action?.data}))
             return {...state,authData: action?.data}
         case LOGOUT:
             localStorage.clear();
-            console.log("reducer action data logout ==>",action?.data);
             return {...state,authData:null}
-
         default:
             return state
     }
